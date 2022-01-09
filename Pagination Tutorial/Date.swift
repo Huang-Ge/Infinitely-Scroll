1d69754e17ddbee8b479324ed7dba0cfb7986f2f
Berry-Huang <gutenragberry@users.noreply.github.com>
2022-01-09 14:54:47 +0100

    Initial Commit

---
+++ Pagination Tutorial/Date.swift
@@ -0,0 +1,12 @@
+//
+//  Date.swift
+//  Pagination Tutorial
+//
+//  Created by Ge Huang on 2022/1/9.
+//
+
+import Foundation
+
+struct Date: Decodable {
+    let year: Int
+}
---
+++ Pagination Tutorial/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  Pagination Tutorial
+//
+//  Created by Ge Huang on 2022/1/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // 1
+        enum TableSection: Int {
+            case dateList
+            case loader
+        }
+    @IBOutlet weak var tableView: UITableView!
+    // 2
+        private let pageLimit = 25
+        private var currentYear: Int = 1970 //start from 1970
+        
+        private var dates = [Date]() {
+            didSet {
+                DispatchQueue.main.async { [weak self] in
+                    self?.tableView.reloadData()
+                }
+            }
+        }
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setupView()
+            fetchData()
+        }
+        
+        private func setupView() {
+            title = "Years"
+            tableView.rowHeight = 64
+            tableView.dataSource = self
+            tableView.delegate = self
+        }
+        
+        // 3
+        private func fetchData() {
+            let data = DateManager.shared.getDate(sinceDate: currentYear)
+            dates.append(contentsOf: data)
+            guard let year = dates.last?.year else { return  }
+            self.currentYear = year + 1
+        }
+    }
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    // 7
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 8
+        guard let listSection = TableSection(rawValue: section) else { return 0 }
+        switch listSection {
+        case .dateList:
+            return dates.count
+        case .loader:
+            return dates.count >= pageLimit ? 1 : 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //guard let section = TableSection(rawValue: indexPath.section) else { return UITableViewCell() }
+        //show data on the cell
+        
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
+        // 9
+        let repo = dates[indexPath.row]
+        cell.textLabel?.text = String(repo.year)
+        cell.detailTextLabel?.text = "\(indexPath.row + 1)"
+        return cell
+    }
+    
+    // fetch generated data
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let section = TableSection(rawValue: indexPath.section) else { return }
+        guard !dates.isEmpty else { return }
+
+        if section == .loader {
+            print("load new data..")
+            fetchData()
+        }
+    }
+    // create loading view
+    private func hideBottomLoader() {
+        DispatchQueue.main.async {
+            let lastListIndexPath = IndexPath(row: self.dates.count - 1, section: TableSection.dateList.rawValue)
+            self.tableView.scrollToRow(at: lastListIndexPath, at: .bottom, animated: true)
+        }
+    }
+}
+
